1511682f528cafb3bf1345e9147453bd2c262256
Crucialjun <nich.otieno@gmail.com>
2022-10-26 23:08:31 +0300

    top row

--- app/src/main/java/com/example/cbconlinecomposed/MainActivity.kt
+++ app/src/main/java/com/example/cbconlinecomposed/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.cbconlinecomposed
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cbconlinecomposed.ui.theme.CBCOnlineComposedTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,9 +31,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        Greeting("Android")
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp)
+                            ){
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = "C.B.C Online",
+                                style = TextStyle(
+                                    color = Color.Blue,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 18.sp
+
+                                )
+                            )
+
+                            Text(
+                                modifier = Modifier.clickable(enabled = true){
+                                    Log.d("Clicked", "onCreate: button clicked")
+                                },
+                                text = "SKIP",
+                                style = TextStyle(
+                                    color = Color.Blue,
+                                    fontWeight = FontWeight.W500,
+                                    fontSize = 16.sp
+
+                                )
+                            )
+
+                        }
+
                     }
 
                 }
